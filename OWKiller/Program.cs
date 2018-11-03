e953c0d3bd0bb7586d8f82d12f76b51311aa44a9
Egor <enjoyhosting@gmail.com>
2018-11-03 08:46:32 -0700

    new

---
+++ OWKiller/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWKiller
+{
+    class Program
+    {
+      static   void kill()
+        {
+            foreach (var process in Process.GetProcessesByName("csgo"))
+            {
+                process.Kill();
+                Console.WriteLine("Trying to kill CSGO");
+            }
+            foreach (var process in Process.GetProcessesByName("Overwatch"))
+            {
+                process.Kill();
+                Console.WriteLine("Trying to kill OW");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            kill();
+            }
+
+   
+    }
+}
